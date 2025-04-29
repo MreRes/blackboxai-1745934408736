a7cfad3a7114f9532e670a5463c47e6fbdbd4869
MreRes <47110975+MreRes@users.noreply.github.com>
2025-04-29 20:47:25 +0700

    Add frontend/src/redux/store.js 1745934445319

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import notificationReducer from './slices/notificationSlice';
+import transactionReducer from './slices/transactionSlice';
+import budgetReducer from './slices/budgetSlice';
+import goalReducer from './slices/goalSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    notifications: notificationReducer,
+    transactions: transactionReducer,
+    budgets: budgetReducer,
+    goals: goalReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ['auth/loginSuccess', 'auth/registerSuccess'],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ['payload.timestamp'],
+        // Ignore these paths in the state
+        ignoredPaths: ['auth.user'],
+      },
+    }),
+});
+
+export default store;
