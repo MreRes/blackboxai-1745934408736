e210c9c55b2b82f0ae24ab8da25b8cb4c76ff18c
MreRes <47110975+MreRes@users.noreply.github.com>
2025-04-29 20:47:06 +0700

    Add frontend/src/index.js 1745934426906

---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import App from './App';
+import store from './redux/store';
+import './index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
